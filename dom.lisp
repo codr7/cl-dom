f7da013e268c63659fdff88201e83857d7706ce4
codr7 <codr7@protonmail.com>
2023-04-22 16:58:34 +0200

    Fix html generation.

--- dom.lisp
+++ dom.lisp
@@ -1,6 +1,6 @@
 (defpackage dom
   (:use cl)
-  (:export *node* body head html new-node node))
+  (:export *node* <a> <body> <head> <html> <new-node> <node> text write-html))
 
 (in-package dom)
 
@@ -12,19 +12,83 @@
   (attrs nil :type list))
 
 (defun new-node (tag &rest attrs)
-  (make-node :tag tag :attrs attrs))
+  (labels ((parse-attrs (in out)
+	     (if in
+		 (let ((k (pop in)) (v (pop in)))
+		   (parse-attrs in (cons (cons k v) out)))
+		 (nreverse out))))
+    (make-node :tag tag :attrs (parse-attrs attrs nil))))
 
-(defun new-child-node (tag &rest attrs)
-  (let ((n (apply #'new-node tag attrs)))
-    (push n (node-children *node*))))
+(defun indent-html (level out)
+  (dotimes (i level)
+    (write-string "  " out)))
 
-(defmacro html (&body body)
-  `(macrolet ((head (&body body)
-		`(let ((*node* (new-child-node :head)))
+(defmethod write-html ((val node) &key (level 0) (out *standard-output*) (pretty? t))
+  (with-slots (tag children attrs) val
+    (when pretty?
+      (indent-html level out))
+    
+    (format out "<~a" tag)
+    
+    (unless (null attrs)
+      (dolist (a attrs)
+	(format out " ~a=\"~a\"" (string-downcase (symbol-name (first a))) (rest a))))
+    
+    (if (null children)
+	(progn
+	  (format out "/>")
+
+	  (when pretty?
+	    (terpri out)))
+	(progn
+	  (format out ">")
+	  
+	  (when pretty?
+	    (terpri out))
+	  
+	  (dolist (cn (reverse children))
+	    (write-html cn :level (1+ level) :out out :pretty? pretty?))
+	  
+	  (when pretty?
+	    (indent-html level out))
+	  
+	  (format out "</~a>" tag)
+	  
+	  (when pretty?
+	    (terpri out))))))
+
+(defmethod write-html ((val string) &key (level 0) (out *standard-output*) (pretty? t))
+  (when pretty?
+    (indent-html level out))
+
+  (write-string val out)
+
+  (when pretty?
+    (terpri out)))
+
+(defmethod print-object ((val node) out)
+  (write-html val :out out))
+
+(defun text (child)
+  (push child (node-children *node*)))
+
+(defmacro with-node ((&rest args) &body body)
+  `(let ((p *node*)
+	 (*node* (new-node ,@args)))
+     (when p
+       (push *node* (node-children p)))
+     ,@body
+     *node*))
+
+(defmacro <html> (&body body)
+  `(macrolet ((<head> (&body body)
+		`(with-node (:head)
 		   ,@body))
-	      (body (&body body)
-		`(let ((*node* (new-child-node :body)))
-		   ,@body)))
-     (let ((*node* (new-node :htmtl)))
-       ,@body
-       *node*)))
+	      (<body> (&body body)
+		`(with-node (:body)
+		   (macrolet ((<a> ((&rest attrs &key href) &body body)
+				`(with-node (:a ,@attrs)
+				   ,@body)))
+		     ,@body))))
+     (with-node (:html)
+       ,@body)))
--- test.lisp
+++ test.lisp
@@ -5,7 +5,11 @@
 (in-package dom-test)
 
 (defun run ()
-  (let ((doc (html
-	      (head)
-	       (body))))
-    (format t "DOC: ~a~%" doc)))
+  (let ((doc (<html>
+	      (<head>)
+	       (<body>
+		(<a> (:href "foo")
+		     (text "bar"))))))
+    (assert (string= "<HTML><HEAD/><BODY><A href=\"foo\">bar</A></BODY></HTML>"
+	     (with-output-to-string (out)
+	       (write-html doc :out out :pretty? nil))))))
